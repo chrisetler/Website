979a7959db58f46b4a2690f82608d04b363f4d21
Chris Etler <cetler123@gmail.com>
2015-07-25 11:00:21 -0400

    refactored javascript file

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+//Put all drivers into here
+function main(event) {
+  $(".welcomeHeader").click(toggleAllSections);
+  $(".sectionHeader").click(toggleASection);
+  $(".listItem").click(toggleProjectDescription);
+}
+$(document).ready(main);
+
+//Toggle everything. Base decisions off of the first section (the about section)
+//If about is hidden, show all. Else hide all
+var toggleAllSections = function(event) {
+  if($("#aboutSection").is(":visible")) {
+    $(".sectionContent").hide();
+  }
+  else{
+    $(".sectionContent").show();
+  }
+}
+
+//Toggle only the secetion whose header was selected
+var toggleASection = function(event) {
+  //for minimizing a section based on its header, the word Header in id can be replaced with Section to get sections ID
+  var thisId = event.currentTarget.id;
+  var childId = "#" + thisId.replace("Header","Section");
+  $(childId).toggle();
+}
+
+//Toggle one of the items in the project list
+var toggleProjectDescription = function(event) {
+  var thisId = event.currentTarget.id;
+  var childId = "#" + thisId.replace("Header","Desc");
+  $(childId).toggle();
+}
